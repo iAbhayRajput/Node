0fc10b447d53dcb1e25889fab50aa8bb49f9e420
iAbhayRajput <126077059+iAbhayRajput@users.noreply.github.com>
2024-04-18 00:35:42 +0530

    Updated validation in  script.js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const app = express()
+
+//using middleware for json to blob conversion and vice versa 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
 //function here act as a middleware 
 app.get('/', function (req, res) {
   res.send('Hey There!, You are at Home')
