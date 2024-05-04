cf29b32b20b4dbfb99116f090c5026ef263ecdd5
iAbhayRajput <126077059+iAbhayRajput@users.noreply.github.com>
2024-05-04 22:53:59 +0530

    Updated app.js
    added routes for upload (profile pic)

--- Mini-project/app.js
+++ Mini-project/app.js
@@ -6,6 +6,8 @@ const userModel = require("./models/user");
 const postModel = require("./models/post");
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const upload = require('./config/multerconfig')
+
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -13,18 +15,74 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,'public')));
 app.use(cookieParser());
 
+
+
 app.get("/", (req,res) =>{
     // res.send("Hey There");
     res.render("index");
+})
+
+app.get("/profile/upload",isLoggedIn, (req,res) =>{
+    res.render("profileupload");
+})
+
+app.post("/upload",isLoggedIn, upload.single('image'), async (req,res) =>{
+    let user = await userModel.findOne({email:req.user.email});
+    user.profilepic = req.file.filename;
+    await user.save();
+    res.redirect("/profile");
 })
 
 app.get("/login", (req,res) =>{
     res.render("login");
 })
 
-app.get("/profile",isLoggedIn, (req,res) =>{
-    console.log(req.user);
-    res.render("login");
+app.get("/profile",isLoggedIn, async (req,res) =>{
+    let user = await userModel.findOne({email: req.user.email}).populate("posts");
+
+    res.render("profile",{user});
+})
+
+app.get("/like/:id",isLoggedIn, async (req,res) =>{
+    let post = await postModel.findOne({_id: req.params.id}).populate("user");
+    
+    if(post.likes.indexOf(req.user.userid) === -1){
+        post.likes.push(req.user.userid);        
+    }
+    else{
+        post.likes.splice(post.likes.indexOf(req.user.userid),1);
+        }
+    
+     await post.save();
+    res.redirect("/profile");
+})
+
+app.get("/edit/:id",isLoggedIn, async (req,res) =>{
+    let post = await postModel.findOne({_id: req.params.id}).populate("user");
+    
+    res.render("edit",{post});
+})
+
+app.post("/update/:id",isLoggedIn, async (req,res) =>{
+    let post = await postModel.findOneAndUpdate({_id: req.params.id},{content: req.body.content});
+    
+    res.redirect("/profile");
+})
+
+
+app.post("/post",isLoggedIn, async (req,res) =>{
+    let user = await userModel.findOne({email: req.user.email});
+    let{content} = req.body;
+
+    let post = await postModel.create({
+        user:user._id,
+        content
+    });
+
+    // console.log(content);
+    user.posts.push(post._id);
+    await user.save();
+    res.redirect("/profile");
 })
 
 app.post("/register", async(req,res) =>{
@@ -58,7 +116,7 @@ app.post("/login", async(req,res) =>{
         if(result){
             let token = jwt.sign({email: email, userid: user._id}, "shhhh");
             res.cookie("token", token);
-         res.status(200).send("you can login");
+         res.status(200).redirect("/profile");
         }
         else res.redirect("/login");
     })
@@ -70,7 +128,7 @@ app.get("/logout", (req,res) =>{
 })
 
 function isLoggedIn(req, res, next){
-    if(req.cookies.token === "") res.send("You must be logged in");
+    if(req.cookies.token === "") res.redirect("/login");
     else{
       let data = jwt.verify(req.cookies.token,"shhhh");
       req.user= data ;
